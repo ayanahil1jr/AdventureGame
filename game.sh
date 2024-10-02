e8caa63d8f1eadb7e2402b7c61aa4834cad3bdbf
ayan <aliayanahil@gmail.com>
2024-10-02 14:06:49 -0500

    Add interactive prompts to game script

--- game.sh
+++ game.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "Welcome to Adventure Game!"
+echo "Choose a location to eplore: forest, castle, or cave."
+read location
+if ["$location" == "forest" ]; then
+	cat forest.txt
+elif [ "$location" == "castle" ]; then
+	cat castle.txt
+elif [ "$location" == "cave" ]; then
+	cat cave.txt
+else
+	echo "Invalid location."
+fi
