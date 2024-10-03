baa50249a2e4d782376e6149d7b0870cd8f15385
ayan <aliayanahil@gmail.com>
2024-10-03 13:30:19 -0500

    Update welcome message in main branch

--- game.sh
+++ game.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Welcome to Adventure Game!"
+echo "Welcome to the Ultimate Adventure Game!"
 echo "Choose a location to eplore: forest, castle, or cave."
 read location
 if ["$location" == "forest" ]; then
