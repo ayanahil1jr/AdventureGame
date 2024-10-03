54e7c0715ff64283427496765928213168b31561
ayan <aliayanahil@gmail.com>
2024-10-03 13:29:04 -0500

    Add monster feature to the game]

--- game.sh
+++ game.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "Welcome to Adventure Game!"
-echo "Choose a location to eplore: forest, castle, or cave."
+echo "Choose a location to explore: forest, castle, or cave."
 read location
-if ["$location" == "forest" ]; then
-	cat forest.txt
-elif [ "$location" == "castle" ]; then
-	cat castle.txt
-elif [ "$location" == "cave" ]; then
-	cat cave.txt
-else
-	echo "Invalid location."
+	if ["$location" == "forest" ]; then
+		cat forest.txt
+	elif [ "$location" == "castle" ]; then
+		cat castle.txt
+	elif [ "$location" == "cave" ]; then
+		cat cave.txt
+		echo "But wait..."
+		cat monster.txt
+	else
+		echo "Invalid location."
 fi
